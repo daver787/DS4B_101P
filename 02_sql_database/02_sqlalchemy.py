3781f007b29185169849bd92db41c60fbe801cb8
David Espinola <daver787@gmail.com>
2022-12-10 09:29:20 -0600

    Created bikes database.

--- 02_sql_database/02_sqlalchemy.py
+++ 02_sql_database/02_sqlalchemy.py
@@ -3,18 +3,42 @@
 
 # IMPORTS ----
 
+import pandas as pd
+import sqlalchemy as sql
+import os as os
+
+os.mkdir("./00_database")
 # CREATING A DATABASE ----
 
 # Instatiate a database
 
+engine = sql.create_engine("sqlite:///00_database/bike_orders_database.sqlite")
+
+conn = engine.connect()
 
 # Read Excel Files
 
-
+bikes_df      = pd.read_excel("00_data_raw/bikes.xlsx")
+bikeshops_df  = pd.read_excel("00_data_raw/bikeshops.xlsx")
+orderlines_df = pd.read_excel("00_data_raw/orderlines.xlsx")
 # Create Tables
 
+bikes_df.to_sql("bikes", con = conn)
 
+pd.read_sql(" SELECT * FROM bikes", con = conn)
+
+bikeshops_df.to_sql("bikeshops", con = conn)
+
+pd.read_sql("SELECT * FROM bikeshops", con = conn)
+
+orderlines_df \
+    .iloc[:,1:]. \
+        to_sql("orderlines", con = conn, if_exists = "replace")
+
+pd.read_sql("SELECT * FROM orderlines", con = conn)
 # Close Connection
+
+conn.close()
 
 # RECONNECTING TO THE DATABASE 
 
