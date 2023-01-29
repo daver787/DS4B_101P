ff5a49664806dca89273285eafe9003d336f0a53
David Espinola <daver787@gmail.com>
2023-01-29 15:24:49 -0600

    Added example functions.

--- 05_programming/05_programming/01_functions.py
+++ 05_programming/05_programming/01_functions.py
@@ -16,15 +16,47 @@ df = collect_data()
 # ?pd.Series.max
 # ?np.max
 
+?pd.Series.max
+
+df.total_price.max()
+
+pd.Series.max("a")
+
+my_max = pd.Series.max
+
+my_max(df.total_price)
+
+type(my_max)
+
+type(pd.Series.max)
 
 # Pandas Data Frame Function
 # ?pd.DataFrame.aggregate
 
+pd.DataFrame.aggregate(
+    self = df,
+    func = np.sum
+)
+
+df.aggregate(func = np.sum)
+
+pd.DataFrame.aggregate(
+    self = df[['total_price']],
+    func = np.quantile,
+    q    = 0.5
+)
+
 # 2.0 OUTLIER DETECTION FUNCTION ----
 # - Works with a Pandas Series
 
+x = df['total_price']
 
 
+def detect_outliers(x):
+    
+    return x
+
+detect_outliers(x)
 
 # 3.0 EXTENDING A CLASS ----
 
