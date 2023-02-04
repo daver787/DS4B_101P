3b041638e2fc11e99e8eabb76def4e8ef59a1fd7
David Espinola <daver787@gmail.com>
2023-02-04 10:13:01 -0600

    Creeated summarize by time function.

--- 05_programming/05_programming/02_func_summarize_by_time.py
+++ 05_programming/05_programming/02_func_summarize_by_time.py
@@ -13,10 +13,13 @@ df = collect_data()
 
 # WHAT WE WANT TO STREAMLINE
 
+rule ='D'
+data = df
+
 df[['category_2', 'order_date', 'total_price']] \
     .set_index('order_date') \
     .groupby('category_2') \
-    .resample('M', kind = 'period') \
+    .resample(rule, kind = 'period') \
     .agg(np.sum) \
     .unstack("category_2") \
     .reset_index() \
@@ -24,6 +27,51 @@ df[['category_2', 'order_date', 'total_price']] \
     .set_index("order_date")
 
 # BUILDING SUMMARIZE BY TIME
+def summarize_by_time(
+    data, date_column,value_column,
+                      groups   = None,
+                      rule     = 'D',
+                      agg_func = np.sum,
+                      kind     = 'timestamp',
+                      *args,
+                      **kwargs):
+    # CHECKS
+    
+    # BODY
+    
+    # Handle date column
+    data = data.set_index(date_column)
+    
+    # Handle groupby
+    if groups is not None:
+        data = data.groupby(groups)
+        
+    # Handle resample
+    data = data.resample(
+        rule = rule,
+        kind = kind
+        
+    )
+    # Handle the aggregration
+    data = data[[value_column]]\
+        .agg(
+        func = agg_func,
+        *args,
+        **kwargs
+    )    
+    
+    return data
+    
+summarize_by_time(
+    data, 
+    date_column  = 'order_date',
+    value_column = 'total_price',
+    #groups       = ['category_1', 'category_2'],
+    rule         = 'M',
+    kind         = 'period'
+    )
+
+
 
 
 # ADDING TO OUR TIME SERIES MODULE
