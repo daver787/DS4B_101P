5f4a661183db19df2046ae5f6e698fddfea5e1ff
David Espinola <daver787@gmail.com>
2023-02-09 13:59:49 -0600

    Added data prep steps before forecasting.

--- 06_sktime/01_sktime_forecast.py
+++ 06_sktime/01_sktime_forecast.py
@@ -7,15 +7,40 @@ import pandas as pd
 import numpy as np
 
 from my_pandas_extensions.database import collect_data
+from my_pandas_extensions.timeseries import summarize_by_time
 
 df = collect_data()
 
 # Sktime Imports
+from sktime.forecasting.arima import AutoARIMA
 
+#?AutoARIMA
 
 # 1.0 DATA SUMMARIZATIONS ----
 
+bike_sales_m_df = df\
+    .summarize_by_time(
+        date_column  = "order_date",
+        value_column = "total_price",
+        rule         = "M",
+        kind         = "period" 
+    )
+    
+bike_sales_cat2_m_df = df\
+    .summarize_by_time(
+        date_column  = "order_date",
+        value_column = "total_price",
+        groups       = ["category_2"],
+        rule         = "M",
+        kind         ="period"
+    )    
+
 # 2.0 SINGLE TIME SERIES FORECAST ----
+
+
+
+
+
 
 # 3.0 MULTIPLE TIME SERIES FORCAST (LOOP) ----
 
