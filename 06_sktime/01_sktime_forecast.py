48f4a10ab01486e816a231c78cf54bc349d46d3b
David Espinola <daver787@gmail.com>
2023-02-10 09:12:48 -0600

    Added autoarima forecast.

--- 06_sktime/01_sktime_forecast.py
+++ 06_sktime/01_sktime_forecast.py
@@ -13,6 +13,7 @@ df = collect_data()
 
 # Sktime Imports
 from sktime.forecasting.arima import AutoARIMA
+from sktime.utils.plotting import plot_series
 
 #?AutoARIMA
 
@@ -37,10 +38,44 @@ bike_sales_cat2_m_df = df\
 
 # 2.0 SINGLE TIME SERIES FORECAST ----
 
+bike_sales_m_df.plot()
 
+bike_sales_m_df
 
+y = bike_sales_m_df['total_price']
 
+forecaster = AutoARIMA(sp = 12)
 
+forecaster.fit(y)
+
+# Prediction
+h = 24
+forecaster.predict(fh = np.arange(1, h+1))
+
+# Confidence Intervals
+predictions_series, conf_int_df = forecaster.predict(
+    fh              = np.arange(1,h+1),
+    return_pred_int = True,
+    alpha           = 0.05
+    )
+
+# type(predictions_ci_tuple)
+
+# predictions_ci_tuple[1]
+
+predictions_series
+conf_int_df
+
+# Visualize
+# ?plot_series
+
+plot_series(
+    y,
+    predictions_series,
+    conf_int_df['lower'],
+    conf_int_df['upper'],
+    labels=['actual', 'predictions','ci_lower','ci_upper']
+    )
 
 # 3.0 MULTIPLE TIME SERIES FORCAST (LOOP) ----
 
