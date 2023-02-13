6f2990e75316b1cb213200fabd1c8c821fe446cd
David Espinola <daver787@gmail.com>
2023-02-13 17:30:41 -0600

    Started building arima forecasting function.

--- 06_sktime/02_arima_automation.py
+++ 06_sktime/02_arima_automation.py
@@ -7,11 +7,70 @@ import pandas as pd
 import numpy as np
 
 from my_pandas_extensions.database import collect_data
+from my_pandas_extensions.timeseries import summarize_by_time
+from sktime.forecasting.arima import AutoARIMA
+from tqdm import tqdm
 
+
+# Workflow
 df = collect_data()
 
-# Sktime Imports
+bike_sales_m_df = df\
+    .summarize_by_time(
+        date_column  = "order_date",
+        value_column = "total_price",
+        rule         = "M",
+        kind         = "period" 
+    )
+    
+bike_sales_cat2_m_df = df\
+    .summarize_by_time(
+        date_column  = "order_date",
+        value_column = "total_price",
+        groups       = ["category_2"],
+        rule         = "M",
+        kind         ="period"
+    ) 
+
 
 # FUNCTION DEVELOPMENT ----
 # - arima_forecast(): Generates ARIMA forecasts for one or more time series.
 
+
+data = bike_sales_cat2_m_df
+
+def arima_forecast(
+    data, h = 12, sp = 12, alpha = 0.05,
+    suppress_warnings = True,
+    *args, **kwargs
+    ):
+   
+   #Checks
+   
+   # Handle inputs ----
+    df = data
+   
+   #FOR LOOP ----
+    model_results_dict ={}
+   
+    for col in tqdm(df.columns, min_interval = 0):
+       # Series Extraction
+       y = df[col]
+       
+   #Modeling
+       forecaster = AutoARIMA(
+       sp                = sp,
+       suppress_warnings = suppress_warnings,
+       *args,
+       **kwargs
+        )
+    
+       forecaster.fit(y)
+       
+       print(forecaster)
+ 
+   #Prediction and Confidence Intervals 
+
+    return None
+
+arima_forecast(data, h =12, sp = 12)
