6ad94f8abaa1414f6c073cd0b53cec9737a41ae7
David Espinola <daver787@gmail.com>
2023-02-20 12:01:07 -0600

    Added code to plotnine basics.

--- 07_visualization/07_visualization/01_plotnine_basics.py
+++ 07_visualization/07_visualization/01_plotnine_basics.py
@@ -2,21 +2,54 @@
 # Module 7 (Plotnine): Plot Anatomy ----
 
 # Imports
+import pandas as pd
+import numpy as np
 
+from my_pandas_extensions.database import collect_data
+from my_pandas_extensions.timeseries import summarize_by_time
+from my_pandas_extensions.forecasting import arima_forecast
+
+from plotnine import *
 
 # Data
+
+df = collect_data()
 
 # VISUALIZATION ----
 
 # Step 1: Data Summarization
 
+bike_sales_y_df = df\
+    .summarize_by_time(
+        date_column  = "order_date",
+        value_column = "total_price",
+        rule         = "Y",
+        kind         = "timestamp"
+    )\
+        .reset_index()
 
 # Step 2: Plot ----
 # - Canvas: Set up column mappings
 # - Geometries: Add geoms
 # - Format: Add scales, labs, theme
 
-
+(
+    # Canvas
+    ggplot(
+        mapping = aes(x = "order_date", y = "total_price"),
+        data    = bike_sales_y_df
+    )
+    
+    #Geometries
+    + geom_col(fill = "#2C3E50")
+    + geom_smooth(
+        method = "lm",
+        se     = False,
+        color  ="dodgerblue"
+        )
+    
+    
+)
 
 # Saving a plot ----
 
