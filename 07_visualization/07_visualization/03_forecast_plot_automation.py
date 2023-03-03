7fa528fda5330c61cb7744ee02e706d37c1c7e72
David Espinola <daver787@gmail.com>
2023-03-02 18:32:58 -0600

    Started plotting function.

--- 07_visualization/07_visualization/03_forecast_plot_automation.py
+++ 07_visualization/07_visualization/03_forecast_plot_automation.py
@@ -2,8 +2,33 @@
 # Module 7 (Plotnine): Plot Automation ----
 
 # Imports
+import pandas as pd
+import numpy as np
+
+from plotnine import *
+from mizani.formatters import dollar_format
+
+from my_pandas_extensions.database import collect_data
+from my_pandas_extensions.timeseries import summarize_by_time
+from my_pandas_extensions.forecasting import arima_forecast
 
 # Workflow until now
+
+df = collect_data()
+
+arima_forecast_df = df\
+    .summarize_by_time(
+       date_column  =  'order_date',
+       value_column =  'total_price',
+       groups       = 'category_1',
+       rule         = 'M',
+       kind         = 'period',
+       wide_format  = True
+    )\
+    .arima_forecast(
+       h  = 12,
+       sp = 12 
+    )      
 
 
 # 1.0 FORECAST VISUALIZATION ----
