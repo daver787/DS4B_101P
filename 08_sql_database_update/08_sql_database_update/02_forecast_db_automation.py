ca6f0492e09c2e60f34e9bfb567c20dd0e59cee4
David Espinola <daver787@gmail.com>
2023-03-08 08:12:59 -0600

    Added database functions to package.

--- 08_sql_database_update/08_sql_database_update/02_forecast_db_automation.py
+++ 08_sql_database_update/08_sql_database_update/02_forecast_db_automation.py
@@ -2,12 +2,37 @@
 # Module 8 (SQL Database Update): Forecast Automation ----
 
 # IMPORTS ----
+import pandas as pd
+import numpy as np
 
+from my_pandas_extensions.database import (
+    collect_data, 
+    write_forecast_to_database,
+    read_forecast_from_database
+)
+
+from my_pandas_extensions.timeseries import summarize_by_time
+from my_pandas_extensions.forecasting import arima_forecast, plot_forecast
+
+df = collect_data()
 
 # 1.0 SUMMARIZE AND FORECAST ----
 
 
 # 1.1 Total Revenue ----
+
+df\
+    .summarize_by_time(
+        date_column  = 'order_date',
+        value_column = 'total_price',
+        rule         = 'M',
+        kind         = 'period'
+    )\
+    .arima_forecast(
+        h  = 12,
+        sp = 12
+    )\
+    .assign(id = 'Total Revenue')    
 
 
 # 1.2 Revenue by Category 1 ----
--- my_pandas_extensions/database.py
+++ my_pandas_extensions/database.py
@@ -4,6 +4,7 @@
 
 import sqlalchemy as sql
 import pandas as pd
+import pandas_flavor as pf
 
 # COLLECT DATA ----
 def collect_data(conn_string = "sqlite:///00_database/bike_orders_database.sqlite"):
@@ -79,3 +80,132 @@ def collect_data(conn_string = "sqlite:///00_database/bike_orders_database.sqlit
     df.columns = df.columns.str.replace(".", "_")
 
     return df
+
+# PREP FORECAST ----
+@pf.register_dataframe_method
+def prep_forecast_data_for_update(
+    data, id_column, date_column
+):
+    # Format the column names
+    df = data\
+        .rename(
+            {
+                id_column   : 'id',
+                date_column : 'date'
+            }, 
+            axis = 1
+            )
+    # Validate correct columns
+    
+    required_col_names = ['id', 'date', 'value','prediction', 'ci_low', 'ci_hi']
+    
+    if not all(pd.Series(required_col_names).isin(df.columns)):
+        col_text = ", ".join(required_col_names)
+        raise Exception(f'Columns must contain: {col_text}')
+    
+    return(df)
+
+# WRITE FORECAST ----
+@pf.register_dataframe_method
+def write_forecast_to_database(
+    data, id_column, date_column, 
+    conn_string = "sqlite:///00_database/bike_orders_database.sqlite",
+    table_name  = "forecast",
+    if_exists   = "fail",
+    **kwargs
+):
+    """Writes the forecast table to the database
+    Args:
+        data (DataFrame):
+            An ARIMA forecast Data Frame. 
+        id_column (str):
+            A single column name specifying a unique identifier for the time series
+        date_column (str):
+            A single column name specifying the date column.
+        conn_string (str, optional):
+            A connection string to database to be updated. Defaults to "sqlite:///00_database/bike_orders_database.sqlite".
+        table_name (str, optional):
+            Table name for the table to be created or modified. Defaults to "forecast".
+        if_exists (str, optional):
+            Used to determine how the table is updated if the table exists. Passed to pandas.to_sql(). Defaults to "fail".
+        **kwargs: Additional arguments passed to pandas.to_sql().
+    See also:
+        - my_pandas_extensions.forecasting.arima_forecast()
+    """
+    # Prepare the data
+    df = prep_forecast_data_for_update(
+    data        = data,
+    id_column   = id_column,
+    date_column = date_column
+    )
+       
+    # Check format for SQL Database   
+    df['date'] = df['date'].dt.to_timestamp()
+    
+    sql_dtype = {
+        "id"         : sql.String(),
+        "date"       : sql.String(),
+        "value"      : sql.Numeric(),
+        "prediction" : sql.Numeric(),
+        "ci_low"     : sql.Numeric(),
+        "ci_hi"      : sql.Numeric()     
+    }
+    
+    # Connect to the Database
+    
+    engine = sql.create_engine(conn_string)
+    
+    conn = engine.connect()
+    
+    df.to_sql(
+        con       = conn,
+        name      = table_name,
+        if_exists = if_exists,
+        dtype     = sql_dtype,
+        index     = False
+        #**kwargs
+        )
+    # Close the connection
+    conn.close()
+    
+    pass
+
+def read_forecast_from_database(
+    conn_string = "sqlite:///00_database/bike_orders_database.sqlite",
+    table_name  = "forecast",
+    **kwargs
+):
+   """Read a forecast from the database
+    Args:
+        conn_string (str, optional):
+            A slqalchemy connection string to find the database. 
+            Defaults to "sqlite:///00_database/bike_orders_database.sqlite".
+        table_name (str, optional): 
+            The SQL table containing the forecast. Defaults to "forecast".
+    Returns:
+        DataFrame: A pandas data frame with the following columns:
+            - id: A unique identifier for the time series
+            - date: A date column
+            - value: The actual values
+            - prediction: The predicted values
+            - ci_low: The lower confidence interval
+            - ci_hi: the upper confidence interval
+    """ 
+   
+   # Connect to Database
+   engine = sql.create_engine(conn_string)
+    
+   conn = engine.connect()
+    
+   # Read from Table
+   
+   df = pd.read_sql(
+       f"SELECT * from {table_name}",
+               con         = conn,
+               parse_dates = ['date']
+               )
+   
+   # Close Connection 
+   conn.close()
+   
+   return df
