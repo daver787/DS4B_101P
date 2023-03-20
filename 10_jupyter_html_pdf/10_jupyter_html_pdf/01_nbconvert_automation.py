ad045feafb71f5df4da0ecadd899e78df326bacc
David Espinola <daver787@gmail.com>
2023-03-19 20:48:36 -0500

    Added code to automate .pdf and .html reports.

--- 10_jupyter_html_pdf/10_jupyter_html_pdf/01_nbconvert_automation.py
+++ 10_jupyter_html_pdf/10_jupyter_html_pdf/01_nbconvert_automation.py
@@ -3,19 +3,37 @@
 
 # IMPORTS ----
 
-
+import glob
+import pathlib
+from tqdm import tqdm
+from traitlets.config import Config
+from nbconvert.preprocessors import TagRemovePreprocessor
+from nbconvert.exporters import HTMLExporter, PDFExporter
+from nbconvert.writers import FilesWriter
 
 # 1.0 NBCONVERT CONFIG ----
 
 # Configure our tag removal
 # - https://nbconvert.readthedocs.io/en/latest/removing_cells.html
 
+c = Config()
 
+c.TemplateExporter.exclude_input = True
+c.TagRemovePreprocessor.remove_cell_tags = ("remove_cell",)
+c.TagRemovePreproccesor.remove_all_outputs_tags = ("remove_output",)
+c.TagRemovePreprocessor.remove_input_tags = ("remove_input",)
+c.TagRemovePreprocessor.enabled = True
 
 # GET FILE LIST ----
 
 # Get file list for conversion
 
+files = glob.glob('09_jupyter_papermill/reports/sales_forecast*.ipynb')
+
+file_path = pathlib.Path(files[0])
+file_path.stem
+file_path.name
+file_path.parents[0]
 
 # 2.0 HTML EXPORT ----
 
