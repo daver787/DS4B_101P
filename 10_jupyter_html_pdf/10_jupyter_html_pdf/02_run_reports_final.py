d173a73c2d47ef0861e6d08837189a7185810b00
David Espinola <daver787@gmail.com>
2023-03-21 21:18:05 -0500

    added in a check for run_reports

--- 10_jupyter_html_pdf/10_jupyter_html_pdf/02_run_reports_final.py
+++ 10_jupyter_html_pdf/10_jupyter_html_pdf/02_run_reports_final.py
@@ -15,7 +15,12 @@ import papermill as pm
 from my_pandas_extensions.database import read_forecast_from_database
 
 # >>> ADD IMPORTS <<<
-
+import glob
+from tqdm import tqdm
+from traitlets.config import Config
+from nbconvert.preprocessors import TagRemovePreprocessor
+from nbconvert.exporters import HTMLExporter, PDFExporter
+from nbconvert.writers import FilesWriter
 
 # COLLECT DATA ----
 
@@ -64,61 +69,15 @@ get_template_path()
 # - Upgraded Reporting Function: Version 2
 # - Add HTML and PDF reporting
 
-def run_reports(data, id_sets=None, 
-                report_titles = None,
-                directory = "reports/"):
 
-    # Make the directory if not created
-
-    dir_path = pathlib.Path(directory)
-    directory_exists = os.path.isdir(dir_path)
-    if not directory_exists:
-        print(f"Making directory at {str(dir_path.absolute())}")
-        os.mkdir(dir_path)
-
-
-    # Make Papermill Jupyter Notebooks
-    for i, id_set in enumerate(id_sets):
-
-        # Input Filename
-        input_path = get_template_path()
-
-        # Output Path
-        report_title = report_titles[i]
-
-        file_name = report_title \
-            .translate(
-                str.maketrans('', '', string.punctuation)
-            ) \
-            .lower() \
-            .replace(" ", "_")
-
-        output_path = pathlib.Path(f'{directory}/{file_name}.ipynb')
-
-        # Parameters
-        params = {
-            'ids': id_set,
-            'title': report_title,
-            'data': data.to_json()
-        }
-
-        # Papermill Execute
-        pm.execute_notebook(
-            input_path  = input_path,
-            output_path = output_path,
-            parameters  = params,
-            report_mode = True
-        )
-
-    #  >>>> NBCONVERT <<<< ----
-
-    # Prep for Conversions
-
-    # Convert to HTML
-
-    # Convert to PDF
-
-    pass 
 
 # Test reporting.py version 2
+run_reports(
+    data            = df,
+    id_sets         = id_sets,
+    report_titles   = titles,
+    directory       = "10_jupyter_html_pdf/sales_reports/",
+    convert_to_html = True,
+    convert_to_pdf  = True
+    )
 
